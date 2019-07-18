07adb99a4d9d10267940e07076873023b6241e35
Stepan Vanecek <vanecek@in.tum.de>
2019-07-18 15:57:00 +0200

    added implementation of sorting/sampling in alphabeta

--- abalone/search-ab-sorted.cpp
+++ abalone/search-ab-sorted.cpp
@@ -9,9 +9,10 @@
 #include "search.h"
 #include "board.h"
 #include "eval.h"
-#include <tuple>
 #include <vector>
 #include <algorithm>
+#include <tuple>
+#include <iostream>
 #include "mpi.h"
 
 
@@ -30,162 +31,154 @@
  * - call finishedNode() when finishing evaluation of a tree node
  * - Use _maxDepth for strength level (maximal level searched in tree)
  */
-class ABSortedStrategy: public SearchStrategy
+class ABStrategySorted: public SearchStrategy
 {
  public:
     // Defines the name of the strategy
-    ABSortedStrategy(): SearchStrategy("AlphaBetaSorted")
-    {
-      indices.reserve(100);
-      values.reserve(100);
-      moveVector.reserve(100);
-      sortedMoveVector.reserve(100);
-    }
+    ABStrategySorted(): SearchStrategy("AlphaBetaSorted/Sampling") {}
 
     // Factory method: just return a new instance of this class
-    SearchStrategy* clone() { return new ABSortedStrategy(); }
+    SearchStrategy* clone() { return new ABStrategySorted(); }
 
  private:
      int nodes_evaluated;
      int branches_cut_off[20];
+
+     int numSamples=16;
+     int threshold=50;
+
     /**
      * Implementation of the strategy.
      */
     int alphaBeta(int depth=0, int alpha = -999999, int beta = 999999);
     void searchBestMove();
 
-    void getMoves(bool);
-    std::vector<int> indices;
-    std::vector<int> values;
-    std::vector<Move> moveVector;
-    std::vector<Move> sortedMoveVector;
-
-
+    std::vector<Move> sampleMoves();
 };
 
-void ABSortedStrategy::getMoves(bool sort)
+
+std::vector<Move> ABStrategySorted::sampleMoves()
 {
-  sortedMoveVector.clear();
-
-  MoveList list;
-  Move move;
-  generateMoves(list);
-  // if (!sort) {
-  //   while (list.getNext(move)) {
-  //     sortedMoveVector.push_back(move);
-  //   }
-  //   return;
-  // }
-
-  indices.clear();
-  values.clear();
-  moveVector.clear();
-
-  int i = 0;
-  while (list.getNext(move)) {
-    moveVector.push_back(move);
-    playMove(move);
-    values.push_back(evaluate());
-    takeBack();
-    indices.push_back(i++);
-  }
-  std::stable_sort(indices.begin(), indices.end(), [this](int i1, int i2) {
-    return this->values[i1] > this->values[i2];
-  });
-  for (auto i: indices) {
-    sortedMoveVector.push_back(moveVector[i]);
-  }
+    std::vector<Move> sortedMoves;
+    MoveList list;
+    Move move;
+    using Value = int;
+    std::vector<std::tuple<Move, Value>> actionValues;
+    generateMoves(list);
+    while (list.getNext(move))
+    {
+        playMove(move);
+        actionValues.push_back(std::make_tuple(move, evaluate()));
+        takeBack();
+    }
+    std::stable_sort(actionValues.begin(), actionValues.end(), [](auto a, auto b) {
+        return std::get<Value>(a) > std::get<Value>(b);
+    });
+    int i;
+    for (i = 0; i < std::min(numSamples, (int)actionValues.size()); i++)
+    {
+        sortedMoves.push_back(std::get<Move>(actionValues[i]));
+    }
+    auto firstVal = std::get<Value>(actionValues[0]);
+    while (true)
+    {
+        if (i >= actionValues.size()) break;
+        Move m; Value val; std::tie(m, val) = actionValues[i];
+        if ((firstVal - val) > threshold) break;
+        sortedMoves.push_back(m);
+        i++;
+    }
+    return sortedMoves;
 }
 
-int ABSortedStrategy::alphaBeta(int depth, int alpha, int beta)
+
+int ABStrategySorted::alphaBeta(int depth, int alpha, int beta)
 {
-    bool max = (depth + 1) % 2;//0=min, 1=max
     if (depth == _maxDepth || !_board->isValid())
     {
         return -(evaluate()-depth);
     }
-
-    //MoveList list;
-    //generateMoves(list);
-    //Move move;
     int bestVal = minEvaluation();
-    int i;
-    if(depth <= (_maxDepth-1))
+
+
+
+    if(depth <= (_maxDepth-2))
     {
-      getMoves(true);
-      for (auto move: sortedMoveVector)
-      {
-          playMove(move);
-          int val = -alphaBeta(depth+1, -beta, -alpha);
-          takeBack();
-          if (val > bestVal) {
-              bestVal = val;
-              if (depth == startingDepth)
-              {
-                  foundBestMove(depth, move, bestVal);
-              }
-          }
+        auto moves = sampleMoves();
+        for (auto move: moves)
+        {
+            playMove(move);
+            int val = -alphaBeta(depth+1, -beta, -alpha);
+            takeBack();
+            if (val > bestVal) {
+                bestVal = val;
+                if (depth == startingDepth)
+                {
+                    foundBestMove(depth, move, bestVal);
+                }
+            }
 
-          if(bestVal > alpha)
-          {
-              //printf("moving  alpha  from %d to %d at depth %d.  Beta is %d\n", alpha, bestVal, depth, beta);
-              alpha = bestVal;
-          }
+            if(bestVal > alpha)
+            {
+                //printf("moving  alpha  from %d to %d at depth %d.  Beta is %d\n", alpha, bestVal, depth, beta);
+                alpha = bestVal;
+            }
 
-          if(alpha >= beta)
-          {
-              branches_cut_off[depth]++;
-              break;
-          }
-      }
+            if(alpha >= beta)
+            {
+                branches_cut_off[depth]++;
+                break;
+            }
+        }
     }
     else
     {
-      MoveList list;
-      generateMoves(list);
-      Move move;
-      for(i = 0; list.getNext(move); i++)
-      {
-          playMove(move);
-          int val = -alphaBeta(depth+1, -beta, -alpha);
-          takeBack();
-          if (val > bestVal) {
-              bestVal = val;
-              if (depth == startingDepth)
-              {
-                  foundBestMove(depth, move, bestVal);
-              }
-          }
+        MoveList list;
+        generateMoves(list);
+        Move move;
+        int i;
+        for(i = 0; list.getNext(move); i++) {
+            playMove(move);
+            int val = -alphaBeta(depth+1, -beta, -alpha);
+            takeBack();
+            if (val > bestVal) {
+                bestVal = val;
+                if (depth == startingDepth)
+                {
+                    foundBestMove(depth, move, bestVal);
+                }
+            }
 
-          if(bestVal > alpha)
-          {
-              //printf("moving  alpha  from %d to %d at depth %d.  Beta is %d\n", alpha, bestVal, depth, beta);
-              alpha = bestVal;
-          }
+            if(bestVal > alpha)
+            {
+                //printf("moving  alpha  from %d to %d at depth %d.  Beta is %d\n", alpha, bestVal, depth, beta);
+                alpha = bestVal;
+            }
 
-          if(alpha >= beta)
-          {
-              branches_cut_off[depth]++;
-              break;
-          }
-      }
+            if(alpha >= beta)
+            {
+                branches_cut_off[depth]++;
+                break;
+            }
+        }
     }
 
     return bestVal;
 }
 
-void ABSortedStrategy::searchBestMove()
+void ABStrategySorted::searchBestMove()
 {
     for(int i = 0; i<_maxDepth; i++)
     {
         branches_cut_off[i] = 0;
     }
-    eval = alphaBeta(startingDepth, -9999999, 9999999);
+    eval = alphaBeta(startingDepth, startingAlpha, startingBeta);
     // for(int i = 0; i<_maxDepth; i++)
     // {
     //     printf("     cut off %d branches at depth %d \n",branches_cut_off[i], i);
     // }
+
 }
 
 // register ourselve as a search strategy
-ABSortedStrategy aBSortedStrategy;
+ABStrategySorted aBStrategySorted;
