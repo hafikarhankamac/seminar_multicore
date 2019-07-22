b9ba29ce110e5d19cf17c979c2c90cf3dcdd61a4
Can Guemeli <guemeli@in.tum.de>
2019-07-22 11:27:34 +0200

    Tournament version

---
+++ abalone2/search-ab.cpp
@@ -0,0 +1,134 @@
+/**
+ * Very simple example strategy:
+ * Search all possible positions reachable via one move,
+ * and return the move leading to best position
+ *
+ * (c) 2006, Josef Weidendorfer
+ */
+#include <stdio.h>
+#include "search.h"
+#include "board.h"
+#include "eval.h"
+#include <tuple>
+#include "mpi.h"
+
+#define TAG_TERMINATE_COMPUTATION 7
+
+#define TERMINATED_BEST_VAL 987654321
+
+/**
+ * To create your own search strategy:
+ * - copy this file into another one,
+ * - change the class name one the name given in constructor,
+ * - adjust clone() to return an instance of your class
+ * - adjust last line of this file to create a global instance
+ *   of your class
+ * - adjust the Makefile to include your class in SEARCH_OBJS
+ * - implement searchBestMove()
+ *
+ * Advises for implementation of searchBestMove():
+ * - call foundBestMove() when finding a best move since search start
+ * - call finishedNode() when finishing evaluation of a tree node
+ * - Use _maxDepth for strength level (maximal level searched in tree)
+ */
+class ABStrategy: public SearchStrategy
+{
+ public:
+    // Defines the name of the strategy
+    ABStrategy(): SearchStrategy("AlphaBeta") {}
+
+    // Factory method: just return a new instance of this class
+    SearchStrategy* clone() { return new ABStrategy(); }
+
+ private:
+     int counter = 0;
+     char tmp_char[2];
+
+     int nodes_evaluated;
+     int branches_cut_off[20];
+    /**
+     * Implementation of the strategy.
+     */
+    int alphaBeta(int depth=0, int alpha = -999999, int beta = 999999);
+    void searchBestMove();
+};
+
+int ABStrategy::alphaBeta(int depth, int alpha, int beta)
+{
+
+    counter++;
+    if(counter%100 == 0)
+    {
+        int message_available;
+        MPI_Status status;
+        MPI_Request message_type;
+        MPI_Iprobe(0, TAG_TERMINATE_COMPUTATION, MPI_COMM_WORLD, &message_available, &status);
+        if(message_available)
+        {
+            // int rank;
+            // MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+            // printf("cutting off worker %d\n", rank);
+            MPI_Irecv(tmp_char, 1, MPI_CHAR, 0, TAG_TERMINATE_COMPUTATION, MPI_COMM_WORLD, &message_type);
+            return TERMINATED_BEST_VAL;
+        }
+    }
+
+
+    if (depth == _maxDepth || !_board->isValid())
+    {
+        return -(evaluate()-depth);
+    }
+
+    MoveList list;
+    generateMoves(list);
+    Move move;
+    int bestVal = minEvaluation();
+    int i;
+    for(i = 0; list.getNext(move); i++) {
+        playMove(move);
+        int val = -alphaBeta(depth+1, -beta, -alpha);
+        if(val == TERMINATED_BEST_VAL)
+        { //if this value returned, exit asap
+            return TERMINATED_BEST_VAL;
+        }
+        takeBack();
+        if (val > bestVal) {
+            bestVal = val;
+            if (depth == startingDepth)
+            {
+                foundBestMove(depth, move, bestVal);
+            }
+        }
+
+        if(bestVal > alpha)
+        {
+            //printf("moving  alpha  from %d to %d at depth %d.  Beta is %d\n", alpha, bestVal, depth, beta);
+            alpha = bestVal;
+        }
+
+        if(alpha >= beta)
+        {
+            branches_cut_off[depth]++;
+            break;
+        }
+    }
+
+    return bestVal;
+}
+
+void ABStrategy::searchBestMove()
+{
+    for(int i = 0; i<_maxDepth; i++)
+    {
+        branches_cut_off[i] = 0;
+    }
+    eval = alphaBeta(startingDepth, startingAlpha, startingBeta);
+    // for(int i = 0; i<_maxDepth; i++)
+    // {
+    //     printf("     cut off %d branches at depth %d \n",branches_cut_off[i], i);
+    // }
+
+}
+
+// register ourselve as a search strategy
+ABStrategy aBStrategy;
